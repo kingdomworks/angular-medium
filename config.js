cb6eaa443d825e89becc697b51dfd88fe525b979
Christopher Pawlukiewicz <cpawlukiewicz@Christophers-Mac-mini.local>
2016-01-07 08:27:33 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  baseURL: "www/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "angular": "github:angular/bower-angular@1.4.8",
+    "typescript": "npm:typescript@1.7.5"
+  }
+});
